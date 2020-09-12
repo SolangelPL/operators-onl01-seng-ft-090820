db0fbd0821cd3da37d9d67b63f5e3b2f1da2fb81
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-12 02:17:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -5,18 +5,12 @@ def unsafe?(speed)
    true
   else 
    false
+  end
 end
 
 
 def not_safe?(speed)
-  if speed > 60
-    true
-  end 
-  elsif speed < 40 
-    true
-  end 
-  else 
-    false
+  
 end
 
     speed > 40 || speed < 60 ? "unsafe" : "not_safe"
